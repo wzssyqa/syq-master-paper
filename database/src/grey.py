366aa3e87bc342fff8e1c0855fa8f8e5d2aef9b4
YunQiang Su <wzssyqa@gmail.com>
2011-10-26 22:37:37 +0800

    fix grey.py

--- database/src/grey.py
+++ database/src/grey.py
@@ -5,13 +5,13 @@ def diff_min_max(x,y):
 	return t.max(),t.min()
 
 def grey(x1,y1,rho=0.5):
-	x=x1/x1.mean()
+	x=x1/(x1.mean())
 	y=numpy.copy(y1)
 	y_m=y1.mean(1)
 	y_len,xxx=y.shape
 	for i in range(0,y_len):
-		y[i]=y[i]/y_m[i]
-
+		y[i]=y[i]/(y_m[i])
+	
 	mnarray=[]
 	mxarray=[]
 	for i in range(0, y_len):
@@ -27,9 +27,9 @@ def grey(x1,y1,rho=0.5):
 		res=(minmin+rho*maxmax)/(diff+rho*maxmax)
 		result.append(res.mean())
 	ret_val=numpy.array(result)
-	return ret_val/(ret_val.mean())
+	return ret_val
 
 if __name__=="__main__":
-    print grey(numpy.array([1,2,3])
-              ,numpy.array([[2,20,4],[4,10,6]]))
+    print grey(numpy.array([1.0,2,3])
+              ,numpy.array([[2.0,5,4],[4.0,10,6]]))
 
